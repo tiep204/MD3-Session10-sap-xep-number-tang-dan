aed95702e93800ec55759b3517f46ab36ad0bbc3
tiep204 <dangtrantiep240604@gmail.com>
2023-06-20 20:57:13 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 4, 2, 1, 5, 7, 34));
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+        for (int num : list) {
+            System.out.print(num + " ");
+        }
+    }
+}
